94ef2b6d2c1da879bd5ccc4cbebdd4e0c09bf4c5
sarvarsaidazov <mrsarvar418@gmail.com>
2024-02-02 23:10:01 +0600

    Changed files

--- lab1/main.swift
+++ lab1/main.swift
@@ -9,91 +9,15 @@ import Foundation
 
 //√(option + v)
 
-FIRST :
-    while (true) {
-    print("Enter first variable")
-    if let firstVarStr = readLine() , let firstVariable = Double (firstVarStr) {
-        SECOND : while (true) {
-            print("Enter second variable")
-            if let secondVarStr = readLine() , let secondVariable = Double (secondVarStr) {
-            MAIN: while (true) {
-                print("Enter operation(+,-,*,/,**,√)")
-                let operation = readLine()
-                switch operation {
-                case "+":
-                    print("Result is \(addition(a : firstVariable,b :secondVariable))")
-                    break MAIN
-                case "-":
-                    print("Result is \(subtraction(a : firstVariable,b :secondVariable))")
-                    break MAIN
-                case "*":
-                    print("Result is \(multiplication(a : firstVariable,b :secondVariable))")
-                    break MAIN
-                case "/":
-                    if secondVariable == 0 {
-                        print("Cant divide by 0.TRY AGAIn")
-                        continue MAIN
-                    } else {
-                        print("Result is \(multiplication(a : firstVariable,b :secondVariable))")
-                        break MAIN
-                    }
-                case "**":
-                    print("Result is \(exponentiation(a : firstVariable,b :secondVariable))")
-                    break MAIN
-                case "√":
-                    if firstVariable < 0 {
-                        print("First var is less than 0 unable to solve.Try Again")
-                        continue FIRST
-                    } else if secondVariable < 0 {
-                        print("Second var is less than 0 unable to solve.Try Again")
-                        continue SECOND
-                    }                        
-                    print("Square root of 1st is \(root(a : firstVariable))")
-                    print("Square root of 2nd is \(root(a : secondVariable))")
-                    break MAIN
-                    
-                default:
-                    print("Wrong Operation.Try Again")
-                    continue
-                }
-            }
-                break SECOND
-            } else {
-                print("Wrong Input.Try Again")
-                continue SECOND
-            }
-        }
-        break FIRST
-    } else {
-        print("Wrong Input.Try Again")
-        continue FIRST
-    }
-}
-
-
-
-func addition(a : Double,b : Double) -> Double {
-    return a + b
-}
-
-func subtraction(a : Double,b : Double) -> Double {
-    return a - b
-}
-
-func multiplication(a : Double,b : Double) -> Double {
-    return a * b
-}
-
-func division(a : Double,b : Double) -> Double {
-    return a / b
-}
-
-func exponentiation(a : Double,b : Double) -> Double {
-    return pow(a, b)
-}
-
-func root(a : Double) -> Double {
-    return sqrt(a)
+print("Chose task(1,2):")
+let choice = readLine()
+switch (choice) {
+    case "1":
+        task1()
+    case "2":
+        task2()
+    default:
+        print("No such task❗️")
 }
 
 
--- lab1/task2.swift
+++ lab1/task2.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+func task2() {
+    print("1)Input First line: ")
+    let firstInput = readLine()
+    print("Input Second line: ")
+    let secondInput = readLine()
+    
+    let combinedString = firstInput!.appending(secondInput!);
+    print("result of concat")
+    print(combinedString)
+    
+    print("2)Input line for reversing")
+    let stringForReverse = readLine()
+    let reversedString = String(stringForReverse!.reversed())
+    print("Reversed string:")
+    print(reversedString)
+    
+    print("3)Input line for count")
+    let stringCounter = readLine()
+    let stringWithoutSpace = stringCounter?.filter {$0 != " "}
+    print("Lentgh wothout spaces: ")
+    print(stringWithoutSpace!.count)
+    
+    print("4)Input line:")
+    if let searchString = readLine() {
+        print("Input substring :")
+        if let substring = readLine() {
+            if let range = searchString.range(of: substring) {
+                print("Substring at index: ")
+                print(searchString.distance(from: searchString.startIndex, to: range.lowerBound) + 1)
+            }
+        }
+    }
+    
+}
